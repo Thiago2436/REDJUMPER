a3b587864e14df301d3a091db50b0d719b4f9613
Thiago Martins e Martins <54513342+Thiago2436@users.noreply.github.com>
2021-10-03 22:11:38 -0300

    Jogo red jumper
    Jogo simples em java script, para treino de logica, front end apenas .

---
+++ R.E.D t Jumpergame/script.js
@@ -0,0 +1,26 @@
+let personagem = document.querySelector('#personagem')
+let quadrado = document.querySelector('#quadrado')
+
+function pular(){
+    if(personagem.classList != 'animar'){
+        personagem.classList.add('animar')
+    }
+
+    setTimeout(function(){
+        personagem.classList.remove('animar')
+    }, 600)
+}
+
+var testarColisao = setInterval(function(){
+    var topoPersonagem = parseInt(
+        window.getComputedStyle(personagem).getPropertyValue('top')
+    )
+    var EsquerdaQuadrado = parseInt(
+        window.getComputedStyle(quadrado).getPropertyValue('left')
+    )
+    if(EsquerdaQuadrado < 20 && EsquerdaQuadrado > 0 && topoPersonagem >= 130){
+        quadrado.style.animatin = 'none'
+        quadrado.style.display = 'none'
+        alert (' RED is DEAD  F5 pra recaregar')
+    }
+}, 10)
